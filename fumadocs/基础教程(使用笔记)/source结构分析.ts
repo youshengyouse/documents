d052619c8ba3ebd2c43efba3e7f4959e7b56cf52
chuse <768287201@qq.com>
2025-09-05 09:52:54 +0800

    2025年9月5日初始化

---
+++ fumadocs/基础教程(使用笔记)/source结构分析.ts
@@ -0,0 +1,37 @@
+import * as tutorials_19 from "../内容/教程/laravel/nova/111/index.mdx?collection=tutorials&hash=1751594597477";
+
+import { _runtime } from "fumadocs-mdx";
+import * as _source from "../source.config";
+export const tutorials = _runtime.docs<typeof _source.tutorials>(
+  [
+    {
+      info: {
+        path: "laravel/nova/111/index.mdx",
+        absolutePath:
+          "/www2024/5_docs/fumadocs/mydoc-fumadocs/内容/教程/laravel/nova/111/index.mdx",
+      },
+      data: tutorials_19,
+    },
+  ],
+  [
+    {
+      info: {
+        path: "react系列/meta.json",
+        absolutePath:
+          "/www2024/5_docs/fumadocs/mydoc-fumadocs/内容/教程/react系列/meta.json",
+      },
+      data: {
+        title: "React系列",
+        pages: [
+          "Fumadocs新手入门/04-样式",
+          "components",
+          "---My Separator---",
+          "./nested/page",
+        ],
+        description: "A demo for Fumadocs OpenAPI",
+        root: true,
+        icon: "Shapes",
+      },
+    },
+  ]
+);
---
+++ nextjs/05-教程/03-实例/02 laravel api/index.md
@@ -0,0 +1,8 @@
+---
+title: laravel api
+---
+
+- https://www.youtube.com/playlist?list=PL1JpS8jP1wgA9EpT9UO0UuPrFcJxGUcxH Laravel REST Api
+- https://www.youtube.com/playlist?list=PL1JpS8jP1wgBgHJBMg9EZ6sR45LaF9cvE Laravel REST API and Next.js
+  https://www.youtube.com/@AngelJayAcademy 他的频道，内容很多
+- https://www.youtube.com/watch?v=4Re7hZdsoOI laravel api与vuejs结合
